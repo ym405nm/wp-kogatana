6dfd1f141c2207dd9d4049d6957abb2d139d92b3
ym405nm <nsd08888@gmail.com>
2017-03-23 13:55:49 +0900

    ログ分析機能

--- wp-kogatana.php
+++ wp-kogatana.php
@@ -1,14 +1,16 @@
 <?php
 /*
-Plugin Name: WP-Kogatana
+Plugin Name: WP-PORTAL プラグイン
 Plugin URI: https://github.com/ym405nm/wp-kogatana
-Description: WP-Kogatana
-Author: yoshinori matsumoto
+Description: WP-PORTAL と連携するプラグインです
+Author: WP-PORTL PROJECT
 Version: 0.1
-Author URI: https://twitter.com/ym405nm
+Author URI: https://wp-portal.net
 */
 
 class WpKogatana {
+
+    public $table_name = 'kogatana';
 
 	function get_portal_icon() {
 		return plugins_url() . "/wp-kogatana/img/portal.png";
@@ -20,24 +22,213 @@ class WpKogatana {
 
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_pages' ) );
-		add_action( 'wp_head', 'add_my_ajaxurl', 1 ); // インライン JavaScript 読み込み
 		add_action( 'wp_ajax_kogatana_update_info', array( __CLASS__, 'kogatana_update_info' ) );
 		add_action( 'wp_ajax_nopriv_kogatana_update_info', array( __CLASS__, 'kogatana_update_info' ) );
-	}
+        add_action('parse_request', array($this, 'handle_request'));
+        add_action('wp_authenticate', array($this, 'handle_login'), 30, 2);
+        register_activation_hook(__FILE__, array($this, 'activate'));
+    }
+
+    function activate()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . $this->table_name;
+        if ($wpdb->get_var("show tables like '%" . $table_name . "%'") != $table_name) {
+            $wpdb->query("
+                CREATE TABLE `" . $table_name . "` (
+                 `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+                 `ipaddr` VARCHAR(25) NOT NULL,
+                 `created_at` DATETIME NOT NULL,
+                 `status` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
+                 `type` VARCHAR(255) NOT NULL,
+                 `result` VARCHAR(255) NOT NULL,
+                 `path` VARCHAR(255) NOT NULL,
+                 `log` VARCHAR(255) NULL,
+                 `password` VARCHAR(255) NULL,
+                 `postdata` TEXT NULL,
+                 PRIMARY KEY (`id`)
+            )");
+        }
+        $default_options = array(
+            "log_send"     => false,
+            "log_password" => false,
+            "slack"        => false,
+            "slack_url"    => "",
+            "slack_token"  => ""
+        );
+        update_option("kogatana_settings", $default_options);
+    }
+
+    function handle_request()
+    {
+        $request_url     = $_SERVER['REQUEST_URI'];
+        $content_dir     = "";
+        $data_type       = "";
+        $plugin_dir_path = WP_CONTENT_DIR;
+        $plugin_split    = explode("/plugins/", $request_url);
+        if (count($plugin_split) > 1) {
+            $data_type     = "plugins";
+            $content_right = $plugin_split[1];
+            $content_dir   = explode("/", $content_right)[0];
+        }
+        $theme_split = explode("/themes/", $request_url);
+        if (count($theme_split) > 1) {
+            $data_type     = "themes";
+            $content_right = $theme_split[1];
+            $content_dir   = explode("/", $content_right)[0];
+        }
+        $target_file_path = $plugin_dir_path . "/" . $data_type . "/" . $content_dir;
+        if (file_exists($target_file_path)) {
+            // 存在する場合
+            $result = "exists";
+        } else {
+            // 存在しない場合
+            $result = "no exists";
+        }
+        if ( ! empty($content_dir)) {
+            WpKogatana::insert_third_party_data($data_type, $content_dir, $result, null);
+            $mes = "$data_type: $content_dir";
+            WpKogatana::post_slack($mes);
+        }
+
+    }
+
+    function handle_login($username, $password)
+    {
+        if (is_null($username) && is_null($password)) {
+            // 通常アクセス
+            return;
+        }
+        $user = wp_authenticate($username, $password);
+        if ( ! is_wp_error($user)) {
+            // ログイン成功した場合
+            return;
+        }
+        $errors = $user->errors;
+        $options = get_option("kogatana_settings");
+        if(!$options["log_password"]){
+            $password = "******";
+        }
+        if (count($errors) > 0) {
+            WpKogatana::insert_login_data($username, $password);
+            $mes = "login: $username,$password";
+            WpKogatana::post_slack($mes);
+        }
+    }
+
+    function post_slack($mes)
+    {
+        $options = get_option("kogatana_settings");
+        if ( ! $options["slack"]) {
+            return false;
+        }
+        $url     = $options["slack_url"];
+        $payload = array("text" => $mes);
+        $ch      = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL            => $url,
+            CURLOPT_POST           => true,
+            CURLOPT_POSTFIELDS     => 'payload=' . json_encode($payload),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_TIMEOUT        => 5,
+        ]);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return true;
+    }
+
+    function insert_login_data($username, $password)
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . $this->table_name;
+        $sql        = $wpdb->prepare(
+            "INSERT INTO `" . $table_name . "`
+            (`created_at`, `ipaddr`, `type`, `result`, `path`, `log`, `password`)
+            VALUES (NOW(), %s, %s, %s, %s, %s, %s)",
+            $_SERVER["REMOTE_ADDR"],
+            'login',
+            'fail',
+            'wp-login.php',
+            $username,
+            $password
+        );
+        $wpdb->query($sql);
+    }
+
+    function insert_third_party_data($data_type, $path, $result, $post_data = null)
+    {
+        global $wpdb;
+        $table_name  = $wpdb->prefix . $this->table_name;
+        $remote_addr = $_SERVER["REMOTE_ADDR"];
+        if (is_null($post_data)) {
+            $sql = $wpdb->prepare(
+                "INSERT INTO `" . $table_name . "`
+            (`created_at`, `ipaddr`, `type`, `result`, `path`)
+            VALUES (NOW(), %s, %s, %s, %s)",
+                $remote_addr,
+                $data_type,
+                $result,
+                $path,
+                $post_data
+            );
+        } else {
+            $sql = $wpdb->prepare(
+                "INSERT INTO `" . $table_name . "`
+            (`created_at`, `ipaddr`, `type`, `result`, `path`, `postdata`)
+            VALUES (NOW(), %s, %s, %s, %s, %s)",
+                $remote_addr,
+                $data_type,
+                $result,
+                $path,
+                $post_data
+            );
+        }
+
+        $wpdb->query($sql);
+
+    }
+
+    function create_output_text($content_dir, $request_url)
+    {
+        $remote_host      = $_SERVER['HTTP_HOST'];
+        $current_date     = new DateTime();
+        $current_datetime = $current_date->format('Y-m-d H:i:s');
+        $output_text      = sprintf(
+            "%s,%s,%s,%s",
+            $current_datetime,
+            $remote_host,
+            $content_dir,
+            $request_url
+        );
+
+        return $output_text;
+    }
+
 
 	function add_pages() {
-		add_menu_page( 'wp-kogatana', 'wp-kogatana', 'level_8', "wp-kogatana", array( $this, 'wp_kogatana_settings' ),
+        add_menu_page('WP PORTAL', 'WP PORTAL', 'level_8', "WP PORTAL", array($this, 'wp_kogatana_settings'),
 			$this->get_portal_icon() );
 	}
 
 	function wp_kogatana_settings() {
+        $post_data  = false;
+        if (array_key_exists("kogatana", $_POST)) {
+            $post_data = WpKogatana::post_data($_POST["kogatana"]);
+        }
 		wp_enqueue_style( "kogatana-default", plugins_url() . "/wp-kogatana/css/kogatana-default.css" );
 		wp_enqueue_style( "kogatana-table", plugins_url() . "/wp-kogatana/css/kogatana-table.css" );
 		$all_plugins = get_plugins();
 		$all_themes  = wp_get_themes();
+        $login_data = $this->get_login_data();
+        $third_data = $this->get_third_party_data();
 		echo "<h1>WP PORTAL 攻撃ログチェッカー</h1>";
 		echo "<div id='link-portal'><span class='dashicons dashicons-share-alt2'></span><a href='https://wp-portal.net' target='_blank'> WP PORTAL にアクセスする</a></div>";
-		echo "<h2>テーマ一覧</h2>";
+        if($post_data){
+            echo "<div class=\"updated fade\"><p><strong>設定を変更しました。</strong></p></div>";
+        }
+		echo "<h2><span class=\"dashicons dashicons-testimonial\"></span> テーマ一覧</h2>";
 		echo "<div class=\"updated fade update-notice themes-update-notice\" ><p>テーマがあるプラグインがあります <a href='/wp-admin/update-core.php'>詳しく</a></p></div>";
 		echo "<table><tr><th>テーマ名</th><th>インストールされているバージョン</th><th>最新バージョン</th><th><img src=\"{$this->get_portal_icon()}\">WP PORTAL</th></tr>";
 		foreach ( $all_themes as $theme => $theme_param ) {
@@ -61,7 +252,7 @@ class WpKogatana {
 EOF;
 		}
 		echo "</table>";
-		echo "<h2>プラグイン一覧</h2>";
+		echo "<h2><span class=\"dashicons dashicons-testimonial\"></span> プラグイン一覧</h2>";
 		echo "<div class=\"updated fade update-notice plugins-update-notice\" ><p>アップデートがあるプラグインがあります <a href='/wp-admin/update-core.php'>詳しく</a></p></div>";
 		echo "<table><tr><th>プラグイン名</th><th>インストールされているバージョン</th><th>最新バージョン</th><th><img src=\"{$this->get_portal_icon()}\">WP PORTAL</th></tr>";
 		foreach ( $all_plugins as $plugin => $plugin_param ) {
@@ -89,13 +280,72 @@ EOF;
 EOF;
 		}
 		echo "</table>";
-		wp_enqueue_script( 'wp-kogatana-default', plugins_url() . '/wp-kogatana/js/default.js', array( 'jquery' ),
+        echo "<h2><span class=\"dashicons dashicons-visibility\"></span> 過去の要注意アクセス</h2>";
+        echo "<h3>ログイン</h3>";
+        echo "<div class=\"updated fade plugins-update-notice\" >
+            <p>過去にログインが失敗したアドレスです。特定のIPアドレスからの情報がないかどうか確認してください。</p>
+        </div>";
+        echo "<table><tr><th>時刻</th><th>IPアドレス</th><th>ユーザ</th><th>パスワード</th></tr>";
+        foreach ($login_data as $login_request) {
+            $output_login_request = sprintf(
+                "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
+                esc_html($login_request->created_at),
+                esc_html($login_request->ipaddr),
+                esc_html($login_request->log),
+                esc_html($login_request->password)
+            );
+            echo $output_login_request;
+        }
+        echo "</table>";
+        echo "<h3>プラグイン/テーマ</h3>";
+        echo "<div class=\"updated fade plugins-update-notice\" >
+            <p>存在しないリソースへのアクセスです。特定のIPアドレスからの情報がないかどうか確認してください。</p>
+        </div>";
+        echo "<table><tr><th>時刻</th><th>IPアドレス</th><th>アクセス先</th><th>製品名</th></tr>";
+        foreach ($third_data as $third_request) {
+            $output_third_request = sprintf(
+                "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
+                esc_html($third_request->created_at),
+                esc_html($third_request->ipaddr),
+                esc_html($third_request->type),
+                esc_html($third_request->path)
+            );
+            echo $output_third_request;
+        }
+        echo "</table>";
+        echo $this->output_form();
+        wp_enqueue_script('wp-kogatana-default', plugins_url() . '/wp-kogatana/js/default.js', array('jquery'),
 			'1.0' );
 	}
 
-	function add_my_ajaxurl() {
-		echo "<script>var ajaxurl = '" . admin_url( 'admin-ajax.php' ) . "';</script>";
-	}
+    function post_data($request_data)
+    {
+        $update_option = array(
+            "log_send"     => false,
+            "log_password" => false,
+            "slack"        => false,
+            "slack_url"    => "",
+            "slack_token"  => ""
+        );
+        if (array_key_exists("log_send", $request_data)) {
+            $update_option["log_send"] = true;
+        }
+        if (array_key_exists("log_password", $request_data)) {
+            $update_option["log_password"] = true;
+        }
+        if (array_key_exists("slack", $request_data)) {
+            $update_option["slack"] = true;
+        }
+        if (array_key_exists("slack_url", $request_data)) {
+            $update_option["slack_url"] = $request_data["slack_url"];
+        }
+        if (array_key_exists("slack_token", $request_data)) {
+            $update_option["slack_token"] = $request_data["slack_token"];
+        }
+        update_option('kogatana_settings', $update_option);
+
+        return true;
+    }
 
 	function kogatana_update_info() {
 		$result    = array();
@@ -123,6 +373,72 @@ EOF;
 		echo json_encode( $result );
 		die();
 	}
+
+    public function output_form()
+    {
+        $options = get_option('kogatana_settings');
+        if ($options["log_send"]) {
+            $log_send = " checked";
+        } else {
+            $log_send = "";
+        }
+        if ($options["log_password"]) {
+            $log_password = " checked";
+        } else {
+            $log_password = "";
+        }
+        if ($options["slack"]) {
+            $slack = " checked";
+        } else {
+            $slack = "";
+        }
+        if ($options["slack_url"] && ! empty($options["slack_url"])) {
+            $slack_url = esc_html($options["slack_url"]);
+        } else {
+            $slack_url = "";
+        }
+        $output_form = <<< EOF
+<h2>設定</h2> 
+<form action="" method="post">
+<input type="checkbox" name="kogatana[log_send]" id="log_send"$log_send><label for="log_send">ログを WP PORTAL へ送信する（データは匿名化され分析後、公開される可能性があります）</label><br><br>
+<input type="checkbox" name="kogatana[log_password]" id="log_password"$log_password><label for="log_password">ログイン試行で使用されたパスワードを保存する（研究・調査目的のみ推奨）</label><br><br>
+<input type="checkbox" name="kogatana[slack]" id="slack"$slack><label for="slack">不信なリクエスト受信時にSlackに投稿する</label> ※ パフォーマンスが低下する恐れがあります<br>
+（
+<label for="slack_url">投稿先 Slack Incoming Hook の URL</label><input type="text" name="kogatana[slack_url]" id="slack_url" value="$slack_url" style="width:600px;">
+）<br><br><br>
+<input type="submit" class="button-primary" value="設定を更新">
+EOF;
+
+        return $output_form;
+    }
+
+    public function get_login_data($limit = 100)
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . $this->table_name;
+        $sql        =
+            "SELECT * FROM `" . $table_name . "`
+            WHERE `type` = 'login'
+            ORDER BY id DESC
+            LIMIT " . $limit;
+        $result     = $wpdb->get_results($sql);
+
+        return $result;
+    }
+
+    public function get_third_party_data($limit = 100)
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . $this->table_name;
+        $sql        =
+            "SELECT * FROM `" . $table_name . "`
+            WHERE `type` = 'themes' OR `type` = 'plugins'
+            ORDER BY id DESC
+            LIMIT " . $limit;
+        $result     = $wpdb->get_results($sql);
+
+        return $result;
+    }
 
 }
 
